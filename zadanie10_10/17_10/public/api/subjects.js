513ac39ee57e64b7eb6f7bfce30bee2877c7168f
maja obidzinska <118509063+majaobi@users.noreply.github.com>
2023-11-06 20:47:36 +0100

    Delete zadanie10_10 directory

--- zadanie10_10/17_10/public/api/subjects.js
+++
@@ -1,7 +0,0 @@
-const subjects = [...Array(10)].map((_, i) => ({
-    id: i + 1,
-    name: `Subject${i + 1}`,
-    hoursAWeek: (i + 1) * 2
-}));
-// z pomocą filipa
-module.exports = subjects
--- zadanie10_10/17_10/public/js/form.js
+++
@@ -1,7 +0,0 @@
-const form = document.querySelector('form');
-
-form.addEventListener('submit', function (e) {
-  //usunięcie blokowania 
-  // e.preventDefault(); 
-  // alert('Wiadomość nie może zostać wysłana!');
-});
