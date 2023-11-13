752d6f7cf93f72231204bfee6d067bd4e4cb110c
kordula <maja.obidzinskaa@icloud.com>
2023-11-13 17:20:41 +0100

    zapytania w prisma

--- zadania31_10/index.js
+++ zadania31_10/index.js
@@ -23,19 +23,26 @@ app.get('/kontakt', (req, res) => {
   res.sendFile(filePath)
 })
 
-app.post('/kontakt', (req, res) => {
-  const { name, email, album, message } = req.body
-  const query = 'INSERT INTO messages (name, email, album, message) VALUES (?, ?, ?, ?)'
-  
-  db.query(query, [name, email, album, message], (err, results) => {
-    if (err) {
-      console.error('Błąd zapisu do bazy danych:', err)
-      res.status(500).json({ error: 'Wystąpił błąd podczas zapisu wiadomości.' })
-    } else {
-      console.log('Wiadomość zapisana w bazie danych')
-      res.redirect('/')
+app.post('/kontakt', async(req, res) => {
+  const {name, email, subject, message} = req.body
+
+  try{
+    const resultMssage = await Prisma.message.create({  
+      data: {
+        name: name,
+        email: email,
+        subject: subject,
+        message: message
+      }
+    })
+    console.log(resultMssage)
+    res.redirect('/')
+  }
+    catch{
+      res.status(500).json({ error: 'Wystąpił błąd podczas pobierania danych.' })
     }
-  })
+
+  
 })
 
 app.get('/api', (req, res) => {
@@ -48,66 +55,64 @@ app.get('/api', (req, res) => {
 })
 
 
-app.get('/api/students', (req, res) => {
-  const query = 'SELECT * FROM students'
-  
-  db.query(query, (err, results) => {
-    if (err) {
-      console.error('Błąd odczytu z bazy danych:', err)
-      res.status(500).json({ error: 'Wystąpił błąd podczas pobierania danych.' })
-    } else {
-      res.json(results)
-    }
-  })
+app.get('/api/students', async(req, res) => {
+ 
+  try{
+    const resultStudents = await Prisma.students.findMany({})
+    console.log(resultStudents)
+    res.json(resultStudents)
+  }
+  catch{
+    res.status(500).json({ error: 'Wystąpił błąd podczas pobierania danych.' })
+  }
+ 
 })
 
 
-app.get('/api/students/:id', (req, res) => {
-  const studentId = req.params.id
-  const query = 'SELECT * FROM students WHERE id = ?'
-  
-  db.query(query, [studentId], (err, results) => {
-    if (err) {
-      console.error('Błąd odczytu z bazy danych:', err)
-      res.status(500).json({ error: 'Wystąpił błąd podczas pobierania danych.' })
-    } else if (results.length === 0) {
-      res.status(404).json({ error: 'Nie znaleziono studenta o podanym ID.' })
-    } else {
-      res.json(results[0])
-    }
-  })
+app.get('/api/students/:id', async(req, res) => {
+ const studentId = req.params.id
+  try{
+    const resultStudents = await Prisma.students.findMany({
+      where: {
+        id: parseInt(studentId)
+      }
+    })
+    console.log(resultStudents)
+    res.json(resultStudents)
+  }
+  catch{
+    res.status(500).json({ error: 'Wystąpił błąd podczas pobierania danych.' })
+  }
 })
 
 
-app.get('/api/subjects', (req, res) => {
-  const query = 'SELECT * FROM subjects'
-  
-  db.query(query, (err, results) => {
-    if (err) {
-      console.error('Błąd odczytu z bazy danych:', err)
-      res.status(500).json({ error: 'Wystąpił błąd podczas pobierania danych.' })
-    } else {
-      res.json(results)
-    }
-  })
-})
+app.get('/api/subjects', async(req, res) => {
+  try{
+    const resultSubjects = await Prisma.subjects.findMany({})
+    console.log(resultSubjects)
+    res.json(resultSubjects)
+  }
+  catch{
+    res.status(500).json({ error: 'Wystąpił błąd podczas pobierania danych.' })
+  }
+}
+)
 
 
-app.get('/api/subjects/:id', (req, res) => {
-  const subjectId = req.params.id
-  const query = 'SELECT * FROM subjects WHERE id = ?'
-
-  
-  db.query(query, [subjectId], (err, results) => {
-    if (err) {
-      console.error('Błąd odczytu z bazy danych:', err)
-      res.status(500).json({ error: 'Wystąpił błąd podczas pobierania danych.' })
-    } else if (results.length === 0) {
-      res.status(404).json({ error: 'Nie znaleziono przedmiotu o podanym ID.' })
-    } else {
-      res.json(results[0])
-    }
-  })
+app.get('/api/subjects/:id', async(req, res) => {
+  const subjectsId = req.params.id
+  try{
+    const resultSubjectsId = await Prisma.students.findMany({
+      where: {
+        id: parseInt(subjectsId)
+      }
+    })
+    console.log(resultSubjectsId)
+    res.json(resultSubjectsId)
+  }
+  catch{
+    res.status(500).json({ error: 'Wystąpił błąd podczas pobierania danych.' })
+  }
 })
   
 
