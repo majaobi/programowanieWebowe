14619a35dd28f0f6088eac31ffb80e22d58233d1
kordula <maja.obidzinskaa@icloud.com>
2023-11-26 21:14:36 +0100

    add  path for post

--- zadania21_11/index.ts
+++ zadania21_11/index.ts
@@ -1,16 +1,20 @@
-import express from 'express';
-import userRouter from './routes/user.routes';
-const app = express();
-const port = 8080;
+import express from 'express'
+import userRouter from './routes/user.routes'
+import profileRouter from './routes/profile.routes'
+import postRouter from './routes/post.routes'
+const app = express()
+const port = 8080
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.set('json spaces', 2);
-app.use(express.static('public'));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.set('json spaces', 2)
+app.use(express.static('public'))
 
-app.use("/users", userRouter);
+app.use("/users", userRouter)
+app.use("/profile", profileRouter)
+app.use("/post", postRouter)
 
 app.listen(port, () => {
-    console.log(`Server is listening at http://localhost:${port}`);
+    console.log(`Server is listening at http://localhost:${port}`)
 });
 
