280480d2205186ab46e0c1d1a054f4562470a144
Maja Obidzińska <118509063+majaobi@users.noreply.github.com>
2024-03-27 00:50:52 +0100

    cors

--- zadania21_11/index.ts
+++ zadania21_11/index.ts
@@ -4,8 +4,17 @@ import profileRouter from './routes/profile.routes'
 import postRouter from './routes/post.routes'
 import CommentRouter from './routes/comment.routes'
 import TagsRouter from './routes/tags.routes'
+import cors from 'cors'
 const app = express()
 const port = 8080
+
+const corsOptions = {
+    origin: "http://localhost:5173",
+    credentials: true,
+    optionsSuccessStatus: 200,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+};
+app.use(cors(corsOptions))
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
